8646f52cb5405ee8f0b5d6e476e6b9b9fbbcf510
cneralich <corrigan@hashicorp.com>
2019-12-03 10:09:17 -0800

    Update gcs.tf

--- examples/bootstrap-google/gcs.tf
+++ examples/bootstrap-google/gcs.tf
@@ -1,4 +1,4 @@
 resource "google_storage_bucket" "tfe-bucket" {
-  name     = "${var.name}-storage-bucket"
+  name     = "${var.name}-storage-bucket-${random_id.db_name_suffix.hex}"
   location = "us"
 }
